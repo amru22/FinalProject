bf5f85427ba43c715a98cc724a0025906b641514
Amelia <amelia.rumjon10@gmail.com>
2024-07-26 13:52:10 +0100

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Scholar on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
